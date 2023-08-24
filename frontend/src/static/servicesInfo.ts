bbca5df6635fd8a84d2016274297d0161507b2cf
unknown <ugasdomingo@gmail.com>
2023-08-24 18:40:39 +0200

    src to alis in static info

--- frontend/src/static/servicesInfo.ts
+++ frontend/src/static/servicesInfo.ts
@@ -3,28 +3,28 @@ export const servicesInfo = [
         title: 'Service 1 - Lorem ipsum',
         description:
             'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae nisl euismod, ult',
-        image: 'src/assets/img/service1.svg',
+        image: '@/assets/img/service1.svg',
         path: '/service1'
     },
     {
         title: 'Service 2 - Lorem ipsum',
         description:
             'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae nisl euismod, ultrices magna quis, aliquam nisl',
-        image: 'src/assets/img/service2.svg',
+        image: '@/assets/img/service2.svg',
         path: '/service2'
     },
     {
         title: 'Service 3 - Lorem ipsum',
         description:
             'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae nisl euismod, ultrices magna quis, aliquam nisl asdfasdf  ',
-        image: 'src/assets/img/service3.svg',
+        image: '@/assets/img/service3.svg',
         path: '/service3'
     },
     {
         title: 'Service 4 - Lorem ipsum',
         description:
             'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae nisl euismod, ultrices magna quis, aliquam nisl',
-        image: 'src/assets/img/service4.svg',
+        image: '@/assets/img/service4.svg',
         path: '/service4'
     }
 ];
